8f714fb6b053f3aa03065552949e66d998bd9eba
zhangkaihongzkh <953628520@qq.com>
2016-08-14 23:36:35 +0800

    之前连接数据库不成功 这次修改部分语句 成功连接上数据库

--- nblog/models/user.js
+++ nblog/models/user.js
@@ -1,12 +1,6 @@
-/**
- * Created by 凯洪 on 2016/8/14.
- */
-/*注册响应*/
-/*处理post请求*/
-
 var mongodb = require('./db');
 
-function User(user){
+function User(user) {
     this.name = user.name;
     this.password = user.password;
     this.email = user.email;
@@ -14,36 +8,34 @@ function User(user){
 
 module.exports = User;
 
-/*User.prototype.save实现保存用户信息 User.get实现用户信息读取*/
 //存储用户信息
-User.prototype.save = function(callback){
-    //要存入数据的用户文档
+User.prototype.save = function(callback) {
+    //要存入数据库的用户文档
     var user = {
         name: this.name,
         password: this.password,
         email: this.email
     };
-
     //打开数据库
-    mongodb.open(function(err, db){
+    mongodb.open(function (err, db) {
         if (err) {
-            return callback(err);   //错误 返回err信息
-        };
-        //读取users集合
-        db.collection('users', function(err, collection){
+            return callback(err);//错误，返回 err 信息
+        }
+        //读取 users 集合
+        db.collection('users', function (err, collection) {
             if (err) {
                 mongodb.close();
-                return callback(err);   //错误 返回err信息
+                return callback(err);//错误，返回 err 信息
             }
-            //将用户数据插入users集合
+            //将用户数据插入 users 集合
             collection.insert(user, {
-                safe:ture
-            }, function(err, user){
+                safe: true
+            }, function (err, user) {
                 mongodb.close();
                 if (err) {
-                    return callback(err);   //错误 返回err信息
+                    return callback(err);//错误，返回 err 信息
                 }
-                callback(null, user[0]);    //成功！err 为 null，并返回存储后的用户文档
+                callback(null, user[0]);//成功！err 为 null，并返回存储后的用户文档
             });
         });
     });
@@ -52,29 +44,26 @@ User.prototype.save = function(callback){
 //读取用户信息
 User.get = function(name, callback) {
     //打开数据库
-    mongodb.open(function(err, db) {
+    mongodb.open(function (err, db) {
         if (err) {
-            return callback(err);
+            return callback(err);//错误，返回 err 信息
         }
-        //读取user集合
-        db.collection('users', function(err, collection) {
+        //读取 users 集合
+        db.collection('users', function (err, collection) {
             if (err) {
                 mongodb.close();
-                return callback(err); //错误 返回err信息
+                return callback(err);//错误，返回 err 信息
             }
-            //查找用户名(name键) 值 作为name的一个文档
+            //查找用户名（name键）值为 name 一个文档
             collection.findOne({
                 name: name
             }, function (err, user) {
                 mongodb.close();
                 if (err) {
-                    return callback(err);   //失败 返回err信息
+                    return callback(err);//失败！返回 err 信息
                 }
-                callback(null, user); //成功 查询用户信息
+                callback(null, user);//成功！返回查询的用户信息
             });
         });
     });
 };
-
-
-
